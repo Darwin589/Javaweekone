e8c67eaa3b5b15cc385c026cef1063bd35e3d998
Darwin589 <chucochicho2009@gmail.com>
2025-02-13 10:18:55 -0500

    Create Name.java

---
+++ Name.java
@@ -0,0 +1,9 @@
+public class Name {
+    public static void main(String[]args) {
+        System.out.println("\n\n\n\n\t\t\t\t Picard");
+        System.out.println("\n\n\n\n\n\t\t\t\t Riker");
+        System.out.println("\n\n\n\t\t\t\t Name     Score");
+        System.out.println("\n\n\n\n\t\t\t\t\t 1874");
+        System.out.println("\n\n\n\n\n\t\t\t\t\t 1789");
+    }
+}
